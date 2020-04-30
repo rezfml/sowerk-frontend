f9c6437f8a2ed2bf97aa9775e5a6814952e8994a
Devinhollister-graham <devin-hg@mihlfeld.com>
2020-04-30 17:45:31 -0500

    Add startup script

--- startup-staging.sh
+++ startup-staging.sh
@@ -3,9 +3,5 @@ echo "$(whoami)"
 export NVM_DIR="/home/master/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 nvm use 10.0.0
-node -v
-npm -v
-nvm which node
-npm install --prefix applications/staging_frontend/public_html
-npm build --prefix applications/staging_frontend/public_html
-npm start --prefix applications/staging_frontend/public_html
+npm install --prefix applications/staging_frontend/public_html --loglevel=error
+npm build --prefix applications/staging_frontend/public_html --loglevel=error
