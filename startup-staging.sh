8cb5c53183f3b553bd01ffeedb0c76060fc32c97
Devinhollister-graham <devin-hg@mihlfeld.com>
2020-05-01 08:58:24 -0500

    Fix run

--- startup-staging.sh
+++ startup-staging.sh
@@ -4,5 +4,5 @@ export NVM_DIR="/home/master/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 nvm use 10.0.0
 npm install --prefix applications/staging_frontend/public_html --loglevel=error
-npm build --prefix applications/staging_frontend/public_html --loglevel=error
+npm run build --prefix applications/staging_frontend/public_html --loglevel=error
 pm2 reload nuxt
