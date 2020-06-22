990acbd0929076de2b8ba6dde87915ae50be1d2d
rez <rez@frontierml.com>
2020-06-22 09:21:56 -0500

    Refactor dashboard home cards

---
+++ components/dashboard/HomeCard.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-card class="white pt-0 my-12">
+    <v-container>
+      <v-card-title style="position: absolute; top: -25px; left: 25px; width: 30%; border-radius: 3px;" class="primary body-2">
+        {{ title }}</v-card-title>
+      <v-card-actions class="d-flex justify-end px-4 py-0">
+        <v-row class="py-0">
+          <v-spacer></v-spacer>
+          <v-col cols="4" class="py-0">
+            <v-text-field
+              label="Search"
+              light
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-card-actions>
+      <v-card-text class="pt-0">
+        <v-simple-table light>
+          <template v-slot:default>
+            <thead>
+            <tr>
+              <th class="text-center primary--text">ID</th>
+              <th class="text-center primary--text">Location Name</th>
+              <th class="text-center primary--text">Address</th>
+              <th class="text-center primary--text">Contact Name</th>
+              <th class="text-center primary--text">Phone</th>
+              <th class="text-center primary--text">Email</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="item in items" :key="item.id">
+              <th class="text-center font-weight-lighter">{{ item.id }}</th>
+              <th class="text-center font-weight-lighter">Lorem ipsum</th>
+              <th class="text-center font-weight-lighter">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</th>
+              <th class="text-center font-weight-lighter">Lorem ipsum</th>
+              <th class="text-center font-weight-lighter">1234567890</th>
+              <th class="text-center font-weight-lighter">lorem@ipsum.com</th>
+            </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+      </v-card-text>
+      <v-card-actions class="d-flex justify-end px-4">
+        <v-btn color="primary" class="px-8">View All</v-btn>
+      </v-card-actions>
+    </v-container>
+  </v-card>
+</template>
+<script>
+  export default {
+    name: 'HomeCard',
+    props: {
+      title: {
+        type: String,
+        required: true
+      },
+      items: {
+        type: Array,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- pages/dashboard/buyer/home.vue
+++ pages/dashboard/buyer/home.vue
@@ -1,9 +1,18 @@
 <template>
   <v-app class="grey lighten-3">
     <v-container class="px-8">
-      <LocationsCard></LocationsCard>
-      <LocationsCard></LocationsCard>
-      <LocationsCard></LocationsCard>
+      <HomeCard
+        :items="items"
+        title="Locations"
+      ></HomeCard>
+      <HomeCard
+        :items="items"
+        title="Quick Provider Lookup"
+      ></HomeCard>
+      <HomeCard
+        :items="items"
+        title="Pending Approved Provider Requests"
+      ></HomeCard>
     </v-container>
   </v-app>
 </template>
@@ -11,13 +20,50 @@
 <script>
   import Sidebar from "~/components/dashboard/buyer/Sidebar";
   import LocationsCard from "~/components/dashboard/LocationsCard";
+  import QuickProviderCard from '~/components/dashboard/QuickProviderCard'
+  import HomeCard from '~/components/dashboard/HomeCard'
 
   export default {
     name: "home",
     layout: "app",
     components: {
       Sidebar,
-      LocationsCard
+      LocationsCard,
+      QuickProviderCard,
+      HomeCard
+    },
+    data() {
+      return {
+        items: [
+          {
+            id: 1,
+            locationName: 'Lorem Ipsum',
+            address: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+            firstName: 'Lorem',
+            lastName: 'Ipsum',
+            phone: '1234567890',
+            email: 'lorem@ipsum.com'
+          },
+          {
+            id: 2,
+            locationName: 'Lorem Ipsum',
+            address: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+            firstName: 'Lorem',
+            lastName: 'Ipsum',
+            phone: '1234567890',
+            email: 'lorem@ipsum.com'
+          },
+          {
+            id: 3,
+            locationName: 'Lorem Ipsum',
+            address: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+            firstName: 'Lorem',
+            lastName: 'Ipsum',
+            phone: '1234567890',
+            email: 'lorem@ipsum.com'
+          }
+        ]
+      }
     }
   };
 </script>
