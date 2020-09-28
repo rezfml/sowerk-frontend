3a33b62de84b0d390fd9d031c4d23b5c30df8569
frontiermedia <jay@frontierml.com>
2020-09-28 11:04:08 -0500

    Register fix

--- pages/register/index.vue
+++ pages/register/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container class="d-flex flex-column pa-0" style="min-height: 1200px; max-width: none!important; background-image: url('https://images.unsplash.com/photo-1530124566582-a618bc2615dc?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9'); background-size: cover; background-position: center;">
+  <v-container class="d-flex flex-column pa-0" style="max-width: none!important; background-image: url('https://images.unsplash.com/photo-1530124566582-a618bc2615dc?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9'); background-size: cover; background-position: center;">
     <v-row class="d-flex flex-column justify-center" style="height: 100vh; background-color: rgba(0,0,0,0.5);">
       <v-card width="100%" max-width="1000px" color="grey" class="darken-4 mx-auto px-6 pt-12" style="border-radius: 20px;">
         <v-card-title class="primary mx-auto" style="width: 80%; position: absolute; top: -30px; left: 10%; border-radius: 20px;"><p class="mx-auto mb-0 text-center white--text" style="font-size: 24px;">Register - Select Account Type</p></v-card-title>
@@ -53,7 +53,7 @@
                     </v-col>
 
                     <v-col cols="12">
-                      <p class="text-center mx-auto mt-4 text-h1" style="max-width: 350px">If you have one property or thousands of assets and would like to manage approved vendors.</p>
+                      <p class="text-center mx-auto mt-4" style="max-width: 350px">If you have one property or thousands of assets and would like to manage approved vendors.</p>
                     </v-col>
 
                     <v-col cols="12">
