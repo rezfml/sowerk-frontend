02d3aa2798611fc8f6ac3d3286855344f51c0ebf
sky7694 <72085456+sky7694@users.noreply.github.com>
2020-11-05 16:51:07 -0600

    legal docs added
    addition of the legal document pages

---
+++ pages/DCMAPolicy/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card style="max-width:90%; margin:auto">
+    <v-card-title>Effective: October 15, 2020</v-card-title>
+    <v-card-text>We take the intellectual property rights of others seriously and require that our Customers and their Authorized Users do the same. The Digital Millennium Copyright Act established a process for addressing claims of copyright infringement that we have implemented for SOWerk services. If you own a copyright or have authority to act on behalf of a copyright owner and want to report a claim that a third party is infringing that material on or through a SOWerk service, please send a notice to our copyright agent that includes all of the items below and we will expeditiously take appropriate action:</v-card-text>
+    <v-list>
+      <v-list-item>1. A description of the copyrighted work that you claim is being infringed;</v-list-item>
+      <v-list-item>2. A description of the material you claim is infringing and that you want removed or access to which you want disabled and the URL or other location of that material;</v-list-item>
+      <v-list-item>3. Your address, telephone number, and email address;</v-list-item>
+      <v-list-item>4. The following statement: “I have a good faith belief that the use of the copyrighted material I am complaining of is not authorized by the copyright owner, its agent, or the law (e.g., as a fair use)”;</v-list-item>
+      <v-list-item>5.The following statement: “The information in this notice is accurate and, under penalty of perjury, I am the owner, or authorized to act on behalf of the owner, of the copyright or of an exclusive right that is allegedly infringed”; and</v-list-item>
+      <v-list-item>6. An electronic or physical signature of the owner of the copyright or a person authorized to act on the owner's behalf.</v-list-item>
+    </v-list>
+    <v-card-text>Our designated copyright agent to receive such claims can be reached as follows:</v-card-text>
+    <v-card-title>By mail:</v-card-title>
+    <v-card-text>SOWerk LLC</v-card-text>
+    <v-card-text>405 N. Jefferson Ave. Suite 1010</v-card-text>
+    <v-card-text>Springfield MO 65807</v-card-text>
+    <v-card-text>United States</v-card-text>
+
+    <v-card-title>By email:<span><a href="mailto:Support@SOWerk.com">Support@SOWerk.com</a></span></v-card-title>
+    <v-card-text>We may, in appropriate circumstances, disable or terminate the accounts of users who may be repeat infringers.</v-card-text>
+    <v-card-text>This process does not limit our ability to pursue any other remedies we may have to address suspected infringement.</v-card-text>
+    </v-card>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
