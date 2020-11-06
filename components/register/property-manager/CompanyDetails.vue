4983802b2b7bc2cf457bbfb6bfdda10737c18fa0
sky7694 <72085456+sky7694@users.noreply.github.com>
2020-11-06 09:43:06 -0600

    email rules update

--- components/register/property-manager/CompanyDetails.vue
+++ components/register/property-manager/CompanyDetails.vue
@@ -254,7 +254,7 @@ export default {
         ],
         emailRules: [
           v => !!v || 'E-mail is required',
-          v => /.+@.[A-Z]+/.test(v) || 'E-mail must be valid',
+          v => /.+@.[a-z]+/.test(v) || 'E-mail must be valid',
           v => (v && v.length <= 100) || 'Email must be less than 100 characters'
         ],
         emailNotRequiredRules: [
