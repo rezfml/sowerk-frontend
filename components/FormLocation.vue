8886da98816524d7ac1e549f30d1a4335122cbae
Brian Griffiths <brian@frontierml.com>
2020-11-09 17:31:28 -0500

    Added another img upload area to the registry page

--- components/FormLocation.vue
+++ components/FormLocation.vue
@@ -344,7 +344,6 @@
       }
     },
     mounted() {
-      console.log("Is this the FormLocation.vue file?gggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg")
       this.formatFullAddress();
     },
     methods: {
@@ -367,12 +366,9 @@
       },
       selectLocationImage(e) {
         this.location.imageUrl = e.target.files[0];
-        console.log(this.location.imageUrl);
         this.locationImageUrl = URL.createObjectURL(this.location.imageUrl);
-        console.log(this.locationImageUrl);
       },
       clickLocationImageUpload() {
-        console.log(this);
         // let imageInput = this.$refs.companyImage;
         // console.log(imageInput);
         // imageInput.$el.click();
