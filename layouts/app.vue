5414404698190839c40f0f11f34aa2d3f39d0294
Jay Leach <leachcoding@gmail.com>
2020-11-10 17:34:22 -0500

    Fixed onboarding styling google sheets comment

--- layouts/app.vue
+++ layouts/app.vue
@@ -48,9 +48,9 @@
           {
             target: '#v-step-0',  // We're using document.querySelector() under the hood
             header: {
-              title: 'Welcome to your SOWerk dashboard!',
+              title: '',
             },
-            content: `<p>This page is specific to you and your account level access.<br/><br/>If you have administrator access your dashboard will show all account locations, all account Vendors, and every Vendor management tool.<br/><br/>However, if you only have staff-level access to a specific location(s) then this dashboard revolves specifically around your needs. Enjoy!
+            content: `<h1>Welcome to your SOWerk dashboard!</h1><p>This page is specific to you and your account level access.<br/>If you have administrator access your dashboard will show all account locations, all account Vendors, and every Vendor management tool.<br/>However, if you only have staff-level access to a specific location(s) then this dashboard revolves specifically around your needs. Enjoy!
 </p>`,
             params: {
               placement: 'bottom',
@@ -60,9 +60,9 @@
           {
             target: '#mylocations',
             header: {
-              title: 'Step 1) Locations Are Critical',
+              title: '',
             },
-            content: `<p>Your SOWerk account is made up of locations. They are places where you will be doing business with Vendors now or in the future and therefore <strong style="text-decoration: underline">super important</strong>. It is essential that you create each property, facility, project, or otherwise unique location so that Vendor vetting and management tools can properly work. </p>`,
+            content: `<h1>Step #1<br/>Locations Are Critical</h1><p>Your SOWerk account is made up of locations. They are places where you will be doing business with Vendors now or in the future and therefore <strong style="text-decoration: underline">super important</strong>. It is essential that you create each property, facility, project, or otherwise unique location so that Vendor vetting and management tools can properly work. </p>`,
             params: {
               placement: "top",
             }
@@ -70,9 +70,9 @@
           {
             target: '#vendors',
             header: {
-              title: 'Step 2) Build Vendor Requirements',
+              title: '',
             },
-            content: `<p>Once locations are established it’s time to define your Vendor requirements. Don’t worry, we make this easy.<br/><br/>You can utilize SOWerk templates to help get you started. The power to vet is all yours! You can completely customize any Vendor application by service/supply category and at each location. If you have the same requirements at all locations administrators can build company templates. </p>`,
+            content: `<h1>Step #2<br/>Build Vendor Requirements</h1><p>Once locations are established it’s time to define your Vendor requirements. Don’t worry, we make this easy.<br/><br/>You can utilize SOWerk templates to help get you started. The power to vet is all yours! You can completely customize any Vendor application by service/supply category and at each location. If you have the same requirements at all locations administrators can build company templates. </p>`,
             params: {
               placement: "top",
             }
@@ -80,9 +80,9 @@
           {
             target: '#vendors',
             header: {
-              title: 'Step 3) Connect With Vendors',
+              title: '',
             },
-            content: `<p>Your locations built and your Vendor requirements are set. It’s time to mix and mingle!<br/><br/>Use the Vendor invite tool to send invitations to those Vendors you wish to apply on SOWerk for your Vendor requirements or invite pre-approved Vendors to connect on SOWerk without application. You can also search the SOWerk Vendor directory to find vendors to invite. </p>`,
+            content: `<h1>Step #3<br/>Connect With Vendors</h1><p>Your locations built and your Vendor requirements are set. It’s time to mix and mingle!<br/><br/>Use the Vendor invite tool to send invitations to those Vendors you wish to apply on SOWerk for your Vendor requirements or invite pre-approved Vendors to connect on SOWerk without application. You can also search the SOWerk Vendor directory to find vendors to invite. </p>`,
             params: {
               placement: "top",
             }
@@ -90,9 +90,9 @@
           {
             target: '#supportfeedback',
             header: {
-              title: 'Step 4) Help & Issues',
+              title: '',
             },
-            content: `<p>If you need help or see an issue we are one click away. We would be honored to support your needs.</p>`,
+            content: `<h1>Step #4<br/>Help & Issues</h1><p>If you need help or see an issue we are one click away. We would be honored to support your needs.</p>`,
             params: {
               placement: "top",
             }
@@ -129,14 +129,22 @@
     align-items: center;
   }
   #v-step-46f34eda .v-step__header {
-    width: 100% !important;
-    font-size: 30px;
-    border-radius: 5px;
+    width: 0% !important;
+    height: 0% !important;
+    visibility: hidden;
   }
   #v-step-46f34eda .v-step__content {
     width: 100% !important;
     font-size: 18px;
     background: #50596c !important;
+  }
+  #v-step-46f34eda .v-step__content h1 {
+    width: 100% !important;
+    font-size: 24px;
+    background: #454d5d !important;
+    padding: 5px 10px 5px 10px;
+    margin: -20px 0px 5px 0px;
+    border-radius: 5px;
   }
   #v-step-46f34eda .v-step__buttons {
     width: 100% !important;
@@ -157,14 +165,22 @@
     align-items: center;
   }
   #v-step-6b1ad225 .v-step__header {
-    width: 100% !important;
-    font-size: 25px;
-    border-radius: 5px;
+    width: 0% !important;
+    height: 0% !important;
+    visibility: hidden;
   }
   #v-step-6b1ad225 .v-step__content {
     width: 100% !important;
     font-size: 15px;
     background: #50596c !important;
+  }
+  #v-step-6b1ad225 .v-step__content h1 {
+    width: 100% !important;
+    font-size: 24px;
+    background: #454d5d !important;
+    padding: 5px 10px 5px 10px;
+    margin: -20px 0px 5px 0px;
+    border-radius: 5px;
   }
   #v-step-6b1ad225 .v-step__buttons {
     width: 100% !important;
@@ -184,14 +200,22 @@
     align-items: center;
   }
   #v-step-bfa546c4 .v-step__header {
-    width: 100% !important;
-    font-size: 25px;
-    border-radius: 5px;
+    width: 0% !important;
+    height: 0% !important;
+    visibility: hidden;
   }
   #v-step-bfa546c4 .v-step__content {
     width: 100% !important;
     font-size: 15px;
     background: #50596c !important;
+  }
+  #v-step-bfa546c4 .v-step__content h1 {
+    width: 100% !important;
+    font-size: 24px;
+    background: #454d5d !important;
+    padding: 5px 10px 5px 10px;
+    margin: -20px 0px 5px 0px;
+    border-radius: 5px;
   }
   #v-step-bfa546c4 .v-step__buttons {
     width: 100% !important;
@@ -211,14 +235,22 @@
     align-items: center;
   }
   #v-step-0fcc61c7 .v-step__header {
-    width: 100% !important;
-    font-size: 25px;
-    border-radius: 5px;
+    width: 0% !important;
+    height: 0% !important;
+    visibility: hidden;
   }
   #v-step-0fcc61c7 .v-step__content {
     width: 100% !important;
     font-size: 15px;
     background: #50596c !important;
+  }
+  #v-step-0fcc61c7 .v-step__content h1 {
+    width: 100% !important;
+    font-size: 24px;
+    background: #454d5d !important;
+    padding: 5px 10px 5px 10px;
+    margin: -20px 0px 5px 0px;
+    border-radius: 5px;
   }
   #v-step-0fcc61c7 .v-step__buttons {
     width: 100% !important;
