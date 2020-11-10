3d849267ca728b9f4a9b52bab974b4455a5fdba4
Jay Leach <leachcoding@gmail.com>
2020-11-10 12:07:19 -0500

    Fixed message filter functionality again, as well as prevented vendors to be requested to apply for what they have already applied to

--- pages/dashboard/vendors/_id.vue
+++ pages/dashboard/vendors/_id.vue
@@ -195,6 +195,7 @@
         reviews: [],
         singleCompanyConnections: [],
         vendorMessages: [],
+        userformsIdForRequest: [],
       }
     },
     async mounted() {
@@ -216,6 +217,7 @@
               for(let i=0; i<response.data.length; i++) {
                 if(location.id === response.data[i].splocations_id && response.data[i].approval_status === 1) {
                   this.connections.push(response.data[i]);
+                  this.userformsIdForRequest.push(response.data[i].pmuserforms_id);
                   if(moment(response.data[i].created).format('L') > moment().subtract(30, 'days').calendar()) {
                     this.connectionsPast30Days.push(response.data[i]);
                   }
@@ -303,7 +305,8 @@
           for(let i=0; i<item.services.length; i++) {
             if(item.services[i].userforms[0] !== 'There are no userforms') {
               for(let j=0; j<item.services[i].userforms.length; j++) {
-                if(item.services[i].userforms[j].applicationStatus !== 0) {
+                // Checks if the application is published public or private. Also checks if the userform is already included in the list of approved forms this vendor has applied for.
+                if(item.services[i].userforms[j].applicationStatus !== 0 && !(this.userformsIdForRequest.includes(item.services[i].userforms[j].id))) {
                   this.userforms.push(item.services[i].userforms[j])
                 }
               }
