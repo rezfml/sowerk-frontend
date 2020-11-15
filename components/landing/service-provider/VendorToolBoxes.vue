b274b32ce60782c3644e61c150cd0e0f65c2f016
Cristina Altreche <c.altreche@gmail.com>
2020-11-14 19:30:04 -0500

    refactor PricingLevels and create Getting Started section

--- components/landing/service-provider/VendorToolBoxes.vue
+++ components/landing/service-provider/VendorToolBoxes.vue
@@ -187,4 +187,22 @@ export default {
 </script>
 
 <style scoped>
+/* .skew-top {
+  position: relative;
+}
+
+.skew-top:before {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  content: '';
+  background: inherit;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  transform-origin: top left;
+  transform: skewY(-2deg);
+  z-index: 20!important;
+} */
 </style>
