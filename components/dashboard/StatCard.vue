3ba6cbde9443c3aeb74abb339355ccbfc795f32d
Brian Griffiths <briangriffiths@brians-mac-mini.lan>
2021-01-05 11:23:58 -0500

    more changes

--- components/dashboard/StatCard.vue
+++ components/dashboard/StatCard.vue
@@ -3,7 +3,7 @@
   <v-container v-if="!$vuetify.breakpoint.xs && !$vuetify.breakpoint.sm" class="d-flex flex-column align-center" style="white-space: pre-wrap !important; background-color: white;">
       <v-card-title class="text-center primary--text font-weight-bold" style="position: absolute; top: 30px;">{{ stat.title }}</v-card-title>
       
-      <v-card-text class="text-center primary--text" style="font-size:4rem;margin-top:60%">{{ stat.value }}</v-card-text>
+      <v-card-text class="text-center primary--text" style="font-size:4rem;margin-top:10%">{{ stat.value }}</v-card-text>
       
       <v-card-actions style="position: absolute; bottom: 10px;">
         <v-btn outlined rounded block color="primary" class="px-16 py-6" style="width: 100% !important; font-size: .8rem;" :to="stat.link">View All</v-btn>
