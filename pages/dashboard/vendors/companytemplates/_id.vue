a35a91073697969ea9f64847ba31591ba3ed165e
Jay Leach <leachcoding@gmail.com>
2021-01-29 00:03:50 -0500

    Save button and edit form overlay added for better user experience

--- pages/dashboard/vendors/companytemplates/_id.vue
+++ pages/dashboard/vendors/companytemplates/_id.vue
@@ -278,20 +278,42 @@
     </transition>
 
     <transition name="slide-fade">
-      <v-card v-if="openEditFormFieldLoad" class="d-flex flex-column align-center justify-center" style="width: 70vw; height: 50vh; position: fixed; left: 25vw; top: 25vh; z-index: 1000;">
-        <v-card-text style="text-align: center; width: 100%; font-size: 24px; color: #A61c00;">Edit Question #{{openEditFormFieldVal.order + 1}} For Form - {{openEditFormFieldVal.name}}</v-card-text>
-        <v-form style="width: 90%;" class="d-flex flex-wrap justify-center">
-          <v-text-field v-model="openEditFormFieldVal.name" class="mx-2" style="width: 45%;" :label="'Question'" :name="openEditFormFieldVal.name"></v-text-field>
-          <v-checkbox v-model="openEditFormFieldVal.required" class="mx-2" style="width: 45%;" :label="'Required Question?'" :name="openEditFormFieldVal.required"></v-checkbox>
-          <v-select :items="typeSelect" v-model="openEditFormFieldVal.type" class="mx-2" style="width: 45%;" :label="'Type of Question'" :name="openEditFormFieldVal.type"></v-select>
-        </v-form>
-        <div style="width: 100%;" class="d-flex justify-space-between">
-          <v-btn @click="deleteSingleFormfield(openEditFormFieldVal)" class="ml-2 mb-2" color="primary" outlined>Delete Form Field</v-btn>
-          <v-btn @click="updateSingleFormfield(openEditFormFieldVal)" class="mr-2 mb-2" color="white" style="background: #707070;" outlined>Update Form Field</v-btn>
-        </div>
-        <v-btn text style="font-size: 30px; position: absolute; right: 10px; top: 10px;" @click="closeEditFormField">X</v-btn>
-      </v-card>
+      <v-divider v-if="loading" style="width: 100%; border: 1px solid #707070"></v-divider>
     </transition>
+
+    <transition name="slide-fade">
+      <v-row v-if="loading" class="d-flex justify-center wrap-row" style="width: 100%;">
+        <v-btn @click="saveUserForm" style="width: 45%;" color="primary" rounded class="mt-n6 mb-2 mx-2 py-8">Save</v-btn>
+        <v-btn :href="'../../vendors/applications'" style="width: 45%;" color="#707070" rounded outlined class="mt-n6 mb-2 mx-2 py-8">Exit</v-btn>
+        <v-progress-circular
+          v-if="saveLoad === false"
+          indeterminate
+          color="primary"
+          :size="30"
+        ></v-progress-circular>
+      </v-row>
+    </transition>
+
+    <v-overlay
+      :absolute="absolute"
+      :value="openEditFormFieldLoad"
+    >
+      <transition name="slide-fade">
+        <v-card v-if="openEditFormFieldLoad" class="d-flex flex-column align-center justify-center" style="width: 70vw; height: 50vh; position: fixed; left: 25vw; top: 25vh; z-index: 1000;">
+          <v-card-text style="text-align: center; width: 100%; font-size: 24px; color: #A61c00;">Edit Question #{{openEditFormFieldVal.order + 1}} For Form - {{openEditFormFieldVal.name}}</v-card-text>
+          <v-form style="width: 90%;" class="d-flex flex-wrap justify-center">
+            <v-text-field v-model="openEditFormFieldVal.name" class="mx-2" style="width: 45%;" :label="'Question'" :name="openEditFormFieldVal.name"></v-text-field>
+            <v-checkbox v-model="openEditFormFieldVal.required" class="mx-2" style="width: 45%;" :label="'Required Question?'" :name="openEditFormFieldVal.required"></v-checkbox>
+            <v-select :items="typeSelect" v-model="openEditFormFieldVal.type" class="mx-2" style="width: 45%;" :label="'Type of Question'" :name="openEditFormFieldVal.type"></v-select>
+          </v-form>
+          <div style="width: 100%;" class="d-flex justify-space-between">
+            <v-btn @click="deleteSingleFormfield(openEditFormFieldVal)" class="ml-2 mb-2" color="primary" outlined>Delete Form Field</v-btn>
+            <v-btn @click="updateSingleFormfield(openEditFormFieldVal)" class="mr-2 mb-2" color="white" style="background: #707070;" outlined>Update Form Field</v-btn>
+          </div>
+          <v-btn text style="font-size: 30px; position: absolute; right: 10px; top: 10px;" @click="closeEditFormField">X</v-btn>
+        </v-card>
+      </transition>
+    </v-overlay>
 
   </v-container>
 </template>
@@ -308,6 +330,7 @@
     },
     data () {
       return {
+        absolute: true,
         vendorType: [
           'Supplier',
           'Servicer'
