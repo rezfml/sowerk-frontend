c8c6a3f759b039ae4221eead870e59b13a9c1193
Jay Leach <leachcoding@gmail.com>
2021-02-02 12:55:43 -0500

    insurance edit

--- components/dashboard/Sidebar.vue
+++ components/dashboard/Sidebar.vue
@@ -404,34 +404,34 @@
               },
             ]
           },
-          // {
-          //   to: '/dashboard/assets/',
-          //   slug: 'assets',
-          //   icon: 'mdi-file-account',
-          //   text: 'Assets',
-          //   id: 'assets',
-          //   children: [
-          //     {
-          //       to: '/dashboard/insurances/',
-          //       slug: 'insurances',
-          //       icon: 'safe-square',
-          //       text: 'Insurances',
-          //       class: 'fixed-bottom',
-          //     },
-          //     {
-          //       to: '/dashboard/licenses',
-          //       slug: 'licenses',
-          //       icon: 'license',
-          //       text: 'Licenses & Certificates',
-          //     },
-          //     {
-          //       to: '/dashboard/vendor-documents',
-          //       slug: 'vendor-documents',
-          //       icon: 'mdi-file',
-          //       text: 'Documents'
-          //     },
-          //   ]
-          // },
+          {
+            to: '/dashboard/assets/',
+            slug: 'assets',
+            icon: 'mdi-file-account',
+            text: 'Assets',
+            id: 'assets',
+            children: [
+              {
+                to: '/dashboard/insurances/',
+                slug: 'insurances',
+                icon: 'safe-square',
+                text: 'Insurances',
+                class: 'fixed-bottom',
+              },
+              {
+                to: '/dashboard/licenses',
+                slug: 'licenses',
+                icon: 'license',
+                text: 'Licenses & Certificates',
+              },
+              {
+                to: '/dashboard/vendor-documents',
+                slug: 'vendor-documents',
+                icon: 'mdi-file',
+                text: 'Documents'
+              },
+            ]
+          },
           {
             to: '/dashboard/profile/',
             slug: 'profile',
